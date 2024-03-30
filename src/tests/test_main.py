c0f1eca404532cc4b05c049d7ce9379bf087f41b
Amakoye <charlesamakoye@gmail.com>
2024-03-30 11:26:57 +0300

    setup tests

--- src/tests/test_main.py
+++ src/tests/test_main.py
@@ -2,3 +2,7 @@ def test_ping(test_app):
     response = test_app.get("/api/test/test-api/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello there"}
+
+
+def test():
+    assert 22 == 22
