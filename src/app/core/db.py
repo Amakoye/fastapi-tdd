4c7694e2dc418cce34487e0cbdcda82fd36c7293
Amakoye <charlesamakoye@gmail.com>
2024-04-08 13:00:27 +0300

    alembic and database setup

---
+++ src/app/core/db.py
@@ -0,0 +1,39 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session, sessionmaker
+from databases import Database
+
+
+load_dotenv(".env")
+
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+
+# SQLAlchemy
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+Base = declarative_base()
+
+
+# Dependency to get the database session
+def get_db_session() -> Session:  # type: ignore
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    db_session = SessionLocal
+    try:
+        yield db_session
+    finally:
+        db_session.close()
+
+
+# Dependency to get the database connection
+def get_db() -> Database:
+    return Database(DATABASE_URL)  # database query builder
+
+
+# Dependency to get the database connection asynchronously
+async def get_async_db() -> Database:
+    return Database(DATABASE_URL)  # database query builder
--- src/app/main.py
+++ src/app/main.py
@@ -3,9 +3,11 @@ import uvicorn
 from app.core.create_app import App
 from app.core.docs import docs_router
 from fastapi.routing import APIRouter
+from app.core.db import engine, metadata
 
 
 BASE_PATH = pathlib.Path(pathlib.Path(__file__).parent.parent)
+
 
 test_router = APIRouter(prefix="/test", tags=["Test endpoint"])
 
@@ -23,7 +25,20 @@ routers = (test_router, docs_router)
 
 instance = App("FastApi Posts", routers, APP_HOME=BASE_PATH, prefix="/api")
 
+metadata.create_all(engine)
+
 app = instance.configure()
+
+
+@app.on_event("startup")
+async def startup():
+    print("Application startup")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    print("Application shutdown")
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, workers=1)
